27c8f21ebb4a1875f1875ef5d18898b176b9570d
titou29430 <148693866+titou29430@users.noreply.github.com>
2025-03-19 12:00:38 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,12 +8,3 @@ if _name_ == "_main_":
     raw_seq = "ACGXTGCA123"
     print("Séquence nettoyée :", nettoyer_sequence(raw_seq))
 
-import unittest
-from src.preprocessing import nettoyer_sequence
-
-class TestPreprocessing(unittest.TestCase):
-    def test_nettoyage(self):
-        self.assertEqual(nettoyer_sequence("ACGXTGCA123"), "ACGTGCA")
-
-if _name_ == '_main_':
-    unittest.main()
